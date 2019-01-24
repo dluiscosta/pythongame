791ec415267687c5c738c346b181a8581e7e845b
Daniel Costa <danielcosta@Daniels-MacBook-Air.local>
2019-01-24 09:09:25 -0200

    encapsulated board class, now can display multiple

--- test.py
+++ test.py
@@ -1,27 +1,85 @@
 import pygame as pg
 
-# Parameters
-grid_size = (20, 15)
-cel_size = 40
-move_steps = 20 #frames required for each movement
-beginning_mov_parcel = 3 #size (1/x %) of the beginning parcel in which a movement it can still be cancelled
-
 # System essentials
 pg.init()
 done = False #to control if the program should end
 clock = pg.time.Clock()
-screen = pg.display.set_mode(
-            (grid_size[0]*cel_size, grid_size[1]*cel_size))
-
-# Setup character
-x, y = (10, 10) #initial position on the grid system
-moving = False #character starts idle
+screen = pg.display.set_mode((1000, 500))
 
 # Movement auxiliars
+moving = False #characters start idle
 most_recent_mov_key = None
 first_mov = False
-def can_move(x, y):
-    return 0 <= x < grid_size[0] and 0 <= y < grid_size[1]
+move_steps = 20 #frames required for each movement
+beginning_mov_parcel = 3 #size (1/x %) of the beginning parcel in which a movement it can still be cancelled
+
+class Board:
+    def __init__(self, rows, cols, char = (0, 0)):
+        self.size = (rows, cols)
+        self.char = char
+
+    # Check if the (x,y) position can be occupied by the character
+    def can_move(self, x, y):
+        return 0 <= x < self.size[0] and 0 <= y < self.size[1]
+
+    # Check if the position in the given direction can be occupied by the character
+    def can_move_dir(self, direction):
+        if direction == pg.K_UP:
+            return self.can_move(self.char[0], self.char[1]-1)
+        elif direction == pg.K_DOWN:
+            return self.can_move(self.char[0], self.char[1]+1)
+        elif direction == pg.K_LEFT:
+            return self.can_move(self.char[0]-1, self.char[1])
+        elif direction == pg.K_RIGHT:
+            return self.can_move(self.char[0]+1, self.char[1])
+        else:
+            raise Exception("Invalid direction.")
+
+    # Updates the character position, if possible
+    def move_char(self, direction):
+        new_pos = list(self.char)
+        if moving[0] == pg.K_UP: new_pos[1] -= 1
+        elif moving[0] == pg.K_DOWN: new_pos[1] += 1
+        elif moving[0] == pg.K_LEFT: new_pos[0] -= 1
+        elif moving[0] == pg.K_RIGHT: new_pos[0] += 1
+        if self.can_move(*new_pos):
+            self.char = new_pos
+
+    # Draws the board and the character, idle or moving (if possible for this board)
+    def draw(self, start_x, start_y, cel_size, moving):
+        if (start_x < 0 or start_y < 0 or
+            (start_x + cel_size*self.size[0]) > screen.get_size()[0] or
+            (start_y + cel_size*self.size[1]) > screen.get_size()[1]):
+           raise Exception("Board can't fit window screen.")
+
+        # Draws grid background
+        pg.draw.rect(screen, (30, 0, 0),
+            pg.Rect(start_x, start_y, self.size[0]*cel_size, self.size[1]*cel_size))
+        for g_x in range(0, self.size[0]):
+            for g_y in range(0, self.size[1]):
+                if (g_x+g_y)%2 == 0:
+                    pg.draw.rect(screen, (80, 0, 0),
+                        pg.Rect(start_x + g_x*cel_size, start_y + g_y*cel_size,
+                                cel_size, cel_size))
+
+        # Draws "character"
+        dx, dy = (0, 0) #delta from current movement
+        if moving:
+            distance_covered = int((cel_size/move_steps) * moving[1][0])
+            if moving[0] == pg.K_UP and self.can_move(self.char[0], self.char[1]-1):
+                dy = - distance_covered
+            elif moving[0] == pg.K_DOWN and self.can_move(self.char[0], self.char[1]+1):
+                dy = + distance_covered
+            elif moving[0] == pg.K_LEFT and self.can_move(self.char[0]-1, self.char[1]):
+                dx = - distance_covered
+            elif moving[0] == pg.K_RIGHT and self.can_move(self.char[0]+1, self.char[1]):
+                dx = + distance_covered
+        cx = start_x + self.char[0]*cel_size + dx
+        cy = start_y + self.char[1]*cel_size + dy
+        pg.draw.rect(screen, (255, 0, 0), pg.Rect(cx, cy, cel_size, cel_size))
+
+
+boards = [Board(20, 20), Board(20, 20, (5, 5))]
 
 # Main loop
 while not done:
@@ -32,8 +90,6 @@ while not done:
             most_recent_mov_key = event.key
             first_mov = True
 
-    d_x, d_y = (x*cel_size, y*cel_size) #display position, can be between cells during movement
-
     # Starts a movement
     # gives precedence to the most recently pressed key.
     if (not moving or
@@ -41,24 +97,25 @@ while not done:
         pressed = pg.key.get_pressed()
 
         #moving = (direction, steps taken)
-        if pressed[pg.K_UP] and most_recent_mov_key == pg.K_UP and can_move(x, y-1):
-            moving = (pg.K_UP, [0])
-        elif pressed[pg.K_DOWN] and most_recent_mov_key == pg.K_DOWN and can_move(x, y+1):
-            moving = (pg.K_DOWN, [0])
-        elif pressed[pg.K_LEFT] and most_recent_mov_key == pg.K_LEFT and can_move(x-1, y):
-            moving = (pg.K_LEFT, [0])
-        elif pressed[pg.K_RIGHT] and most_recent_mov_key == pg.K_RIGHT and can_move(x+1, y):
-            moving = (pg.K_RIGHT, [0])
+        if pressed[pg.K_UP] and most_recent_mov_key == pg.K_UP:
+            if any([board.can_move_dir(pg.K_UP) for board in boards]):
+                moving = (pg.K_UP, [0])
+        elif pressed[pg.K_DOWN] and most_recent_mov_key == pg.K_DOWN:
+            if any([board.can_move_dir(pg.K_DOWN) for board in boards]):
+                moving = (pg.K_DOWN, [0])
+        elif pressed[pg.K_LEFT] and most_recent_mov_key == pg.K_LEFT:
+            if any([board.can_move_dir(pg.K_LEFT) for board in boards]):
+                moving = (pg.K_LEFT, [0])
+        elif pressed[pg.K_RIGHT] and most_recent_mov_key == pg.K_RIGHT:
+            if any([board.can_move_dir(pg.K_RIGHT) for board in boards]):
+                moving = (pg.K_RIGHT, [0])
 
     # Continues or finishes a movement
     if moving:
         if moving[1][0] == move_steps: #finishes
-            # Updates the grid position
-            if moving[0] == pg.K_UP: y -= 1
-            elif moving[0] == pg.K_DOWN: y += 1
-            elif moving[0] == pg.K_LEFT: x -= 1
-            elif moving[0] == pg.K_RIGHT: x += 1
-            d_x, d_y = (x*cel_size, y*cel_size) #recalc display position
+            # Updates the characters positions
+            for board in boards:
+                board.move_char(moving[0])
             first_mov = False
             moving = False
         else: #continues
@@ -69,23 +126,11 @@ while not done:
                     moving = False # cancels movement
             if moving:
                 moving[1][0] += 1 #takes one more step in defined direction
-                # Calculates display position
-                distance_covered = int((cel_size/move_steps) * moving[1][0])
-                if moving[0] == pg.K_UP: d_y = y*cel_size - distance_covered
-                elif moving[0] == pg.K_DOWN: d_y = y*cel_size + distance_covered
-                elif moving[0] == pg.K_LEFT: d_x = x*cel_size - distance_covered
-                elif moving[0] == pg.K_RIGHT: d_x = x*cel_size + distance_covered
 
-    # Draws grid background
+    # Draws screen and boards
     screen.fill((0, 0, 0))
-    for g_x in range(0, grid_size[0]):
-        for g_y in range(0, grid_size[1]):
-            if (g_x+g_y)%2 == 0:
-                pg.draw.rect(screen, (40, 0, 0),
-                    pg.Rect(g_x*cel_size, g_y*cel_size, cel_size, cel_size))
-
-    # Draws "character"
-    pg.draw.rect(screen, (255, 0, 0), pg.Rect(d_x, d_y, cel_size, cel_size))
+    boards[0].draw(50, 50, 20, moving)
+    boards[1].draw(550, 50, 20, moving)
 
     pg.display.flip() #flips buffers, updating screen
     clock.tick(60) #waits for the time assigned for a frame
